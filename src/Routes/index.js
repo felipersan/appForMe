657b01dac0b1685253e0914841e43581c24b45a0
felipersan <feliperodrigues.san@gmail.com>
2022-05-15 11:31:29 -0300

    icones na tabBar

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -6,14 +6,40 @@ import Frases from '../pages/Page1';
 import User from '../pages/user';
 import Setting from '../pages/config';
 
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 const BottomTab = createBottomTabNavigator();
 
 export default function RoutesTabNavigator() {
   return (
     <BottomTab.Navigator>
-      <BottomTab.Screen name="Setting" component={Setting} />
-      <BottomTab.Screen name="Frases" component={Frases} />
-      <BottomTab.Screen name="User" component={User} />
+      <BottomTab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <FontAwesome name="cogs" color="#121212" size={20} />;
+          },
+        }}
+      />
+      <BottomTab.Screen
+        name="Frases"
+        component={Frases}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <FontAwesome name="leaf" color="#121212" size={20} />;
+          },
+        }}
+      />
+      <BottomTab.Screen
+        name="User"
+        component={User}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <FontAwesome name="user" color="#121212" size={20} />;
+          },
+        }}
+      />
     </BottomTab.Navigator>
   );
 }
